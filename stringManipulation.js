3de60e03a9890ea18872018ee55bbf560762d2d6
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-01-12 11:32:20 +0530

    strign manipulation added

---
+++ stringManipulation.js
@@ -0,0 +1,7 @@
+let string = "Hello world"
+console.log(string.length)
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+console.log(string.indexOf("world"))
+console.log(string.slice(0, 5))
+console.log(string.charAt(2))
